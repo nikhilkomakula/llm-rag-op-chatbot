961137fe5f70253985b0b15ac2a0acf82934d26b
Nikhil Komakula <nikhil.komakula@gmail.com>
2024-04-13 09:10:17 +0530

    Minor changes and fixes

--- src/generation/generate_response.py
+++ src/generation/generate_response.py
@@ -6,7 +6,7 @@ from typing import Optional
 from src.retrieval.retriever_chain import get_base_retriever, load_hf_llm, create_qa_chain, create_qa_chain_eval
 
 # constants
-HF_MODEL        = "huggingfaceh4/zephyr-7b-alpha"  # "mistralai/Mistral-7B-Instruct-v0.2" # "google/gemma-7b"
+HF_MODEL        = "huggingfaceh4/zephyr-7b-beta"  # "mistralai/Mistral-7B-Instruct-v0.2" # "google/gemma-7b"
 EMBEDDING_MODEL = "BAAI/bge-large-en-v1.5"
 
 
